81c7a22a3f24b017a0963f5bad1fcc9dbd9f7ad8
crisswebapp <crisswebapp@gmail.com>
2020-03-29 17:16:55 -0600

    Primer Guardado

---
+++ components/footer/footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>Criss Web App</p>
+      </div>
+      <div class="column has-text-right">
+        <p>Vuemmerce | Made with ❤</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter'
+}
+</script>
+
+<style scoped>
+.footer {
+  background: #b9e2fc;
+  color: black;
+}
+</style>

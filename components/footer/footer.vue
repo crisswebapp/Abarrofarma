73d7ee67733ad2d4d9f86cfd8aa86e0746f081b3
crisswebapp <41885675+crisswebapp@users.noreply.github.com>
2020-04-01 23:30:53 -0600

    Update footer.vue
    Agregando padless

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer color="indigo" dark app>
+  <v-footer color="indigo" dark app padless>
     <v-row>
       <v-col cols="12" class="text-center">
         <p class="white--text">

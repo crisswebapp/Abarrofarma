c0e5ec4f5a810871765079da59c3c794ad422f34
crisswebapp <crisswebapp@gmail.com>
2020-04-05 21:54:22 -0600

    modified:   pages/farma/index.vue

--- pages/farma/index.vue
+++ pages/farma/index.vue
@@ -1,24 +1,19 @@
 <template>
   <v-container fluid>
     <v-card>
-      <v-card-title>Lista Productos</v-card-title>
-      <v-snackbar
-        v-model="snackbar"
-        :bottom="y === 'bottom'"
-        :color="color"
-        :left="x === 'left'"
-        :multi-line="mode === 'multi-line'"
-        :right="x === 'right'"
-        :timeout="timeout"
-        :top="y === 'top'"
-        :vertical="mode === 'vertical'"
-      >
-        {{ error }}
-        <v-btn dark text @click="snackbar = false">
-          Close
-        </v-btn>
-      </v-snackbar>
+      <v-card-title>Abarrofarma</v-card-title>
     </v-card>
+    <v-snackbar
+      v-model="noti.activar"
+      :color="noti.color"
+      :timeout="noti.timeout"
+      :top="true"
+    >
+      {{ noti.msg }}
+      <v-btn dark text @click="noti.activar = false">
+        Close
+      </v-btn>
+    </v-snackbar>
     <v-row align="center" justify="center">
       <ContenedorBuscador @click="hacerBusqueda" />
       <ResultadoBusqueda
@@ -36,26 +31,28 @@ import ContnedorProductos from '~/components/abarrofarma/contenedorProductos'
 import ResultadoBusqueda from '~/components/abarrofarma/resultadoBusqueda'
 import ContenedorBuscador from '~/components/abarrofarma/contenedorBuscador'
 export default {
-  components: { ContnedorProductos, ResultadoBusqueda, ContenedorBuscador },
+  components: {
+    ContnedorProductos,
+    ResultadoBusqueda,
+    ContenedorBuscador
+  },
   data() {
     return {
-      color: 'error',
-      mode: '',
-      timeout: 2000,
-      x: null,
-      y: 'top',
-      snackbar: false,
-      error: null,
-      dataText: null,
       productos: null,
-      infoBusqueda: {}
+      infoBusqueda: {},
+      noti: {
+        msg: '',
+        color: 'error',
+        timeout: 2500,
+        activar: false
+      }
     }
   },
   created: function() {
     return this.generarData()
   },
   methods: {
-    generarData: function() {
+    generarData() {
       this.productos = data().hijos[0].hijos.map(prodcuto => {
         let newprod = {}
         newprod['nombre'] = prodcuto.valor.nombre
@@ -63,7 +60,7 @@ export default {
         return newprod
       })
     },
-    hacerBusqueda: function(data, tBusqueda) {
+    hacerBusqueda(data, tBusqueda) {
       this.infoBusqueda = {}
       try {
         switch (tBusqueda) {
@@ -72,14 +69,14 @@ export default {
               data.toLowerCase().trim(),
               this.productos
             )
-            this.snackbar = false
+            this.noti.activar = false
             break
           case 'Busqueda Binaria':
             console.log('Busqueda Binaria')
         }
       } catch (error) {
-        this.snackbar = true
-        this.error = error
+        this.noti.activar = true
+        this.noti.msg = error
       }
     }
   }

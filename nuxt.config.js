f411cb30fd879aca44d4952112dca4b938178ab4
crisswebap <crisswebapp@gmail.com>
2020-04-19 15:16:17 -0600

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,6 @@ export default {
    */
   css: [
       // Load a Node.js module directly (here it's a Sass file)
-      'bulma',
       // CSS file in the project
       '@/assets/css/main.css',
       // SCSS file in the project
